07731da3e53fdd59fe61e248c5661df41269860f
Philip Phan <phantriphuoc@yahoo.com>
2023-05-25 22:05:23 -0400

    adding framework change

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const splitSlider = document.getElementById("splitSlider");
+const hitSlider = document.getElementById("hitSlider");
+/* splitSlider.style.display = "none"; */
+//Takes in array of buttons that need to be disabled
+function disableButtons(a){
+    for (let i = 0; i < a.length; i++){
+        let element = document.getElementById(a[0]);
+        element.style.display = "none";
+    }
+}
+function changeButtons(){
+    if (!player.turn){
+        let a = ["hit", "split", "splitSlider", "hitSlider"]
+        disableButtons(a);
+    }
+    else{
+    }
+}
+gameState.subscribe(changeButtons);
 
-splitSlider.style.display = "none";
-while (true){
-    if (player.turn){
-        
+
+function startGame(){
+    //need to set random player/opponent's turn to true
+    if (Math.random() * 2 > 1){
+        player.turn = true;
     }
-    else if (opponent.turn){
-        
+    else{
+        opponent.turn = true;
     }
+    progress();
+}
+function progress(){
+    if (winner || !player.alive || !opponent.alive){
+        window.requestAnimationFrame(progress);
+    }
+    gameState.update();
 }
---
+++ utils/utils.js
@@ -0,0 +1,60 @@
+class Player{
+    constructor(opponent=null, hand1=1, hand2=1, turn=false){
+        this.hand1 = hand1;
+        this.hand2 = hand2;
+        this.opponent = opponent;
+        this.turn = turn;
+        this.alive = true;
+        
+    }
+    //ourHand: int, opponentHand: String [key]
+    hit(ourHand, opponentHand){
+        this.opponent[opponentHand] += ourHand;
+    }
+    //Assume value1 + value2 = hand1 + hand2
+    split(value1, value2){
+        this.hand1 = value1;
+        this.hand2 = value2;
+    }
+    update(){
+        this.hand1 = this.hand1 >= 5? 0: this.hand1;
+        this.hand2 = this.hand2 >= 5? 0: this.hand2;
+        this.alive = (this.hand1 == 0 && this.hand2 == 0)? false: true;
+    }
+    
+}
+class Observer0{
+    constructor(){
+        this.clients = [];
+        this.subscribe = function(f){
+            this.clients.push(f);
+        }
+        this.update = function(){
+            this.clients.forEach(x => x());
+        }
+    }
+}
+
+var winner = null;
+function checkOpponent(player){
+    return function(){
+        if (player.opponent.alive == false){
+            winner = player;
+        }
+    }
+}
+
+
+
+
+const player = new Player();
+const opponent = new Player();
+player.opponent = opponent;
+opponent.opponent = player;
+const gameState = new Observer0();
+gameState.subscribe(() => player.update());
+gameState.subscribe(() => opponent.update());
+
+gameState.subscribe(checkOpponent(player));
+gameState.subscribe(checkOpponent(opponent));
+
